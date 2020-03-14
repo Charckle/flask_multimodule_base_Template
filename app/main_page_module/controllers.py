7695c55fd972c74a09c8e8c5dba0f3a73eda5e34
charckle <andrej.zubin@email.com>
2020-03-14 18:11:38 +0100

    first commit

---
+++ app/main_page_module/controllers.py
@@ -0,0 +1,73 @@
+# Import flask dependencies
+from flask import Blueprint, request, render_template, \
+                  flash, g, session, redirect, url_for
+# Import the database object from the main app module
+from app import db
+
+# Import module forms
+from app.main_page_module.forms import LoginForm, RegisterForm
+
+# Import module models (i.e. User)
+from app.main_page_module.models import User
+
+
+# Define the blueprint: 'auth', set its url prefix: app.url/auth
+main_page_module = Blueprint('main_page_module', __name__, url_prefix='/')
+
+
+# Set the route and accepted methods
+@main_page_module.route('/', methods=['GET', 'POST'])
+def index():
+
+    return render_template("main_page_module/index.html")
+
+# Set the route and accepted methods
+@main_page_module.route('/login/', methods=['GET', 'POST'])
+def login():
+
+    # If sign in form is submitted
+    form = LoginForm(request.form)
+
+    # Verify the sign in form
+    if form.validate_on_submit():
+
+        user = User.query.filter_by(username=form.username_or_email.data).first()
+        if not user:
+            user = User.query.filter_by(email=form.username_or_email.data).first()
+
+        if user and user.check_password(form.password.data):
+
+            session['user_id'] = user.id
+
+            flash('Welcome %s' % user.name, 'success')
+            
+            return redirect(url_for('main_page_module.index'))
+
+        flash('Wrong email or password', 'error')
+
+    return render_template("main_page_module/auth/login.html", form=form)
+
+@main_page_module.route('/logout/')
+def logout():
+    session.pop("user_id", None)
+    
+    flash('You have been logged out. Have a nice day!', 'success')
+
+    return redirect(url_for("main_page_module.login"))
+
+# Set the route and accepted methods
+@main_page_module.route('/register/', methods=['GET', 'POST'])
+def register():
+    #insert check, if the user is already logged in
+    form = RegisterForm(request.form)
+    print(form.username.data)
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data, password = form.password.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        
+        flash('Congratulations, you are now a registered user!', 'success')
+        
+        return redirect(url_for('main_page_module.login'))
+    return render_template('main_page_module/auth/register.html', title='Register', form=form)
---
+++ app/main_page_module/models.py
@@ -0,0 +1,56 @@
+# Import the database object (db) from the main application module
+from app import db
+
+# Import password / encryption helper tools
+from werkzeug import check_password_hash, generate_password_hash
+
+# Define a base model for other database tables to inherit
+class Base(db.Model):
+
+    __abstract__  = True
+
+    id = db.Column(db.Integer, primary_key=True)
+    date_created  = db.Column(db.DateTime,  default=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime,  default=db.func.current_timestamp(),
+                              onupdate=db.func.current_timestamp())
+
+# Define a User model
+class User(Base):
+
+    __tablename__ = 'users_table'
+
+    # User Name
+    name    = db.Column(db.String(128),  nullable=False)
+    
+    # UserName
+    username    = db.Column(db.String(128),  nullable=False,
+                            unique=True)    
+
+    # Identification Data: email & password
+    email    = db.Column(db.String(128),  nullable=False,
+                         unique=True)
+    password = db.Column(db.String(192),  nullable=False)
+
+    # Authorisation Data: role & status
+    role     = db.Column(db.SmallInteger, nullable=False)
+    status   = db.Column(db.SmallInteger, nullable=False)
+    
+
+    # New instance instantiation procedure
+    def __init__(self, username, email, password):
+
+        self.name     = username
+        self.username = username
+        self.email    = email
+        self.password = password
+        self.role = 0
+        self.status = 1
+
+    def __repr__(self):
+        return '<User %r>' % (self.name)
+    
+    def set_password(self, password):
+            self.password = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password, password)    
